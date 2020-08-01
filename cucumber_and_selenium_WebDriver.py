4210d7bf3736448644d9728f2378349df7d15ea7
rocketpy <dimitr.ushakov@gmail.com>
2020-08-01 23:29:13 +0300

    updated

--- cucumber_and_selenium_WebDriver.py
+++ cucumber_and_selenium_WebDriver.py
@@ -1,1 +1,16 @@
+#  taked from :  https://medium.com/@hmurari/bdd-cucumber-and-selenium-webdriver-based-test-automation-framework-in-python-ae092a7581d3
+pip install behave
+pip install selenium
 
+
+@given(u'I load the website')
+def step_impl(context):
+  raise NotImplementedError(u'STEP: Given I load the website')
+  
+@when(u'I go to "Dashboard" page')
+def step_impl(context):
+  raise NotImplementedError(u'STEP: When I go to "Dashboard" page')
+  
+@then(u'I see this component "Status"')
+def step_impl(context):
+  raise NotImplementedError(u'STEP: Then I see this component "Status"')
