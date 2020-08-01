ec6fdc8caf09c0d80e1cb09ebaa6b966ff4af47f
rocketpy <dimitr.ushakov@gmail.com>
2020-08-01 23:30:11 +0300

    updated

--- cucumber_and_selenium_WebDriver.py
+++ cucumber_and_selenium_WebDriver.py
@@ -14,3 +14,20 @@ def step_impl(context):
 @then(u'I see this component "Status"')
 def step_impl(context):
   raise NotImplementedError(u'STEP: Then I see this component "Status"')
+
+  
+from behave import given, when, then
+
+
+@given(u'I load the website')
+def step_impl_load_website(context):
+  print('I load the website')
+  
+@when(u'I go to "{page}" page')
+def step_impl_goto_page(context, page):
+  print('I go to {} page'.format(page))
+  
+@then(u'I see this component "{component}"')
+  def step_impl_verify_component(context, component):
+  print('I see this component "{}"'.format(component))
+  
