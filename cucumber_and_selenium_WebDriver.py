37d18f7a41713ca6231cb03495bc3d9b48736262
rocketpy <dimitr.ushakov@gmail.com>
2020-08-02 23:55:13 +0300

    updated

--- cucumber_and_selenium_WebDriver.py
+++ cucumber_and_selenium_WebDriver.py
@@ -31,3 +31,18 @@ def step_impl_goto_page(context, page):
   def step_impl_verify_component(context, component):
   print('I see this component "{}"'.format(component))
   
+  
+from behave import given, when, then
+
+
+@then(u'Dashboard Status shows correct values for row "{row}"')
+def step_impl_dashboard_status(context, row):
+  print('Dashboard Status for row {}'.format(row))
+  
+@then(u'Clicking on Status Refresh should refresh status component')
+def step_impl_status_refresh(context):
+  print('Status Refresh button')
+  
+@then(u'Dashboard Battery shows Battery or AC Power with correct icon.')
+def step_impl_battery_status(context):
+  print('Dashboard battery status')
