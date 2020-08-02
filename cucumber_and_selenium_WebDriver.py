b65e65ef84b30f6c7208d771d65ca6e9f7a120f0
rocketpy <dimitr.ushakov@gmail.com>
2020-08-02 23:57:50 +0300

    modified

--- cucumber_and_selenium_WebDriver.py
+++ cucumber_and_selenium_WebDriver.py
@@ -5,15 +5,15 @@ pip install selenium
 
 @given(u'I load the website')
 def step_impl(context):
-  raise NotImplementedError(u'STEP: Given I load the website')
+    raise NotImplementedError(u'STEP: Given I load the website')
   
 @when(u'I go to "Dashboard" page')
 def step_impl(context):
-  raise NotImplementedError(u'STEP: When I go to "Dashboard" page')
+    raise NotImplementedError(u'STEP: When I go to "Dashboard" page')
   
 @then(u'I see this component "Status"')
 def step_impl(context):
-  raise NotImplementedError(u'STEP: Then I see this component "Status"')
+    raise NotImplementedError(u'STEP: Then I see this component "Status"')
 
   
 from behave import given, when, then
@@ -21,15 +21,15 @@ from behave import given, when, then
 
 @given(u'I load the website')
 def step_impl_load_website(context):
-  print('I load the website')
+    print('I load the website')
   
 @when(u'I go to "{page}" page')
 def step_impl_goto_page(context, page):
-  print('I go to {} page'.format(page))
+    print('I go to {} page'.format(page))
   
 @then(u'I see this component "{component}"')
-  def step_impl_verify_component(context, component):
-  print('I see this component "{}"'.format(component))
+def step_impl_verify_component(context, component):
+    print('I see this component "{}"'.format(component))
   
   
 from behave import given, when, then
@@ -37,12 +37,24 @@ from behave import given, when, then
 
 @then(u'Dashboard Status shows correct values for row "{row}"')
 def step_impl_dashboard_status(context, row):
-  print('Dashboard Status for row {}'.format(row))
+    print('Dashboard Status for row {}'.format(row))
   
 @then(u'Clicking on Status Refresh should refresh status component')
 def step_impl_status_refresh(context):
-  print('Status Refresh button')
+    print('Status Refresh button')
   
 @then(u'Dashboard Battery shows Battery or AC Power with correct icon.')
 def step_impl_battery_status(context):
-  print('Dashboard battery status')
+    print('Dashboard battery status')
+
+@then(u'Clicking on Battery Refresh should refresh battery component')
+def step_impl_battery_refresh(context):
+    print('Battery refresh')
+  
+@then(u'Dashboard Settings shows correct values for row "{row}"')
+def step_impl_settings(context, row):
+    print('Dashboard settings for {}'.format(row))
+  
+@then(u'Dashboard GPS shows correct values for row "{row}"')
+def step_impl_gps_settings(context, row):
+    print('Dashboard GPS row: {}'.format(row))
