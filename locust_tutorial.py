d6cc05a030ce706922ca5f5563bb558abb8fefbf
rocketpy <dimitr.ushakov@gmail.com>
2020-08-03 22:31:13 +0300

    updated

--- locust_tutorial.py
+++ locust_tutorial.py
@@ -3,4 +3,19 @@
 
 #  pip install locust
 
+#  creating a Performance Test 
+from locust import HttpLocust, TaskSet, task
 
+
+class LoginPage(TaskSet):
+    @task
+    def login_page_with_response_code_assertion(self):
+        r = self.client.get("/login/")
+        assert r.status_code is 200, "Unexpected response code: " + r.status_code
+class LoginUser(HttpLocust):
+    task_set = LoginPage
+    host = "http://127.0.0.1:8000"
+    min_wait = 1000
+    max_wait = 5000
+
+    
