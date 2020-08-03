33e99a62f5b343c1a47e17bbd1d927220c4d51f0
rocketpy <dimitr.ushakov@gmail.com>
2020-08-03 23:50:27 +0300

    updated

--- locust_tutorial.py
+++ locust_tutorial.py
@@ -8,6 +8,8 @@
 
 #  pip install locust
 
+# to start locust in master mode:  locust -f my_locustfile.py --master
+
 #  run the test saved as locustfile.py , in the terminal:  locust --host=http://127.0.0.1:8000
 
 #  creating a Performance Test 
@@ -47,3 +49,52 @@ class WebsiteUser(HttpUser):
     @task
     def about(self):
         self.client.get("/about/")
+        
+        
+# this example will simulate a user(from off. website)
+
+import random
+from locust import HttpUser, between, task
+from pyquery import PyQuery
+
+
+class AwesomeUser(HttpUser):
+    host = "https://docs.locust.io/en/latest/"
+    
+    # we assume someone who is browsing the Locust docs, 
+    # generally has a quite long waiting time (between 
+    # 10 and 600 seconds), since there's a bunch of text 
+    # on each page
+    wait_time = between(10, 600)
+    
+    def on_start(self):
+        # start by waiting so that the simulated users 
+        # won't all arrive at the same time
+        self.wait()
+        # assume all users arrive at the index page
+        self.index_page()
+        self.urls_on_current_page = self.toc_urls
+    
+    @task(10)
+    def index_page(self):
+        r = self.client.get("")
+        pq = PyQuery(r.content)
+        link_elements = pq(".toctree-wrapper a.internal")
+        self.toc_urls = [
+            l.attrib["href"] for l in link_elements
+        ]
+    
+    @task(50)
+    def load_page(self):
+        url = random.choice(self.toc_urls)
+        r = self.client.get(url)
+        pq = PyQuery(r.content)
+        link_elements = pq("a.internal")
+        self.urls_on_current_page = [
+            l.attrib["href"] for l in link_elements
+        ]
+    
+    @task(30)
+    def load_sub_page(self):
+        url = random.choice(self.urls_on_current_page)
+        r = self.client.get(url)
