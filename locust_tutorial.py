9f821e206e29c66406edeebbb8f734f7c68c092e
rocketpy <dimitr.ushakov@gmail.com>
2020-08-03 23:39:21 +0300

    updated

--- locust_tutorial.py
+++ locust_tutorial.py
@@ -26,3 +26,24 @@ class LoginUser(HttpLocust):
     max_wait = 5000
 
     
+#  other example (from official website)
+from locust import HttpUser, between, task
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 15)
+    
+    def on_start(self):
+        self.client.post("/login", {
+            "username": "test_user",
+            "password": ""
+        })
+    
+    @task
+    def index(self):
+        self.client.get("/")
+        self.client.get("/static/assets.js")
+        
+    @task
+    def about(self):
+        self.client.get("/about/")
