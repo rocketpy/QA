25c7707d9412900f1259962e490801054f46a7bf
rocketpy <dimitr.ushakov@gmail.com>
2020-08-04 23:34:32 +0300

    updated

--- testing_unittest_selenium.py
+++ testing_unittest_selenium.py
@@ -1,1 +1,15 @@
+import unittest
+import requests
+from selenium import webdriver
+
+
+class SeleniumCBT(unittest.TestCase):
+    def setUp(self):
+
+        self.username = "user@email.com"
+        self.authkey  = "secret_key"
+
+        self.api_session = requests.Session()
+        self.api_session.auth = (self.username,self.authkey)
+        self.test_result = None
 
