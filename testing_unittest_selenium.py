2c80fe1c1db14ac8001d65bd5fca07acc80b7b7f
rocketpy <dimitr.ushakov@gmail.com>
2020-08-05 23:27:05 +0300

    updated

--- testing_unittest_selenium.py
+++ testing_unittest_selenium.py
@@ -13,3 +13,15 @@ class SeleniumCBT(unittest.TestCase):
         self.api_session.auth = (self.username,self.authkey)
         self.test_result = None
 
+        caps = {}
+        caps['browserName'] = 'Chrome'
+        caps['version'] = '60x64'
+        caps['platform'] = 'Windows 10'
+        caps['screenResolution'] = '1366x768'
+        caps['record_video'] = 'true'
+
+        self.driver = webdriver.Remote(desired_capabilities=caps,
+                                       command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub"%(self.username,self.authkey)
+                                      )
+        
+        
