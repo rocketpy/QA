ddc1eae0ebe70aa8003da22432736760a372a70a
rocketpy <dimitr.ushakov@gmail.com>
2020-08-09 20:09:31 +0300

    updated

--- auto_testing.py
+++ auto_testing.py
@@ -1,4 +1,7 @@
+#  docs: https://pypi.org/project/seleniumbase/
+
 #  pip install seleniumbas
+
 from seleniumbase import BaseCase
 
 
@@ -6,4 +9,18 @@ class MyTestClass(BaseCase):
     
     def test_my_site(self):
         self.demo_mode = True
+        self.demo_sleep = 0.5
+        self.message_duration = 2.5
+        self.open("https://.../search/")
         
+        title = "Bla bla bla"  # send_key
+        
+        self.type('[placeholder]', title + "\n")  # self.type(SELECTOR, TEXT)  # Type text (Add "\n" to text for pressing enter/return.)
+        self.click("link=%s", title)
+        self.assert_element("div.nav-content [id]")
+        self.assert_text(title, "h1")
+        self.highlight("div.description div.abstract")
+        self.highlight("h2")
+        
+        h3 = "h3:nth-of-type(%s)" 
+                            
