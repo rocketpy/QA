16d67f04a877e64b207f0ec4671b260e362cdc24
rocketpy <dimitr.ushakov@gmail.com>
2020-08-11 22:57:24 +0300

    updated

--- stepik/snippets.py
+++ stepik/snippets.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 browser = webdriver.Chrome()
 
 browser.get("https://...")
 submit_button = browser.find_element_by_id("submit")  # find a button by ID
+# or
+submit_button = browser.find_element(By.ID, "submit")  # using a universal method:  find_element()
+
