8aa85c055ea292dd1d1b437405658c6d150d27e0
rocketpy <dimitr.ushakov@gmail.com>
2020-08-12 16:30:59 +0300

    added a main code

--- stepik/find_elements_by.py
+++ stepik/find_elements_by.py
@@ -1,1 +1,27 @@
+import time
+from selenium import webdriver
 
+
+browser.get("https://fake-shop.com/book1.html")
+
+# adding unit (goods) to cart
+add_button = browser.find_element_by_css_selector(".add")
+add_button.click()
+
+# open second unit page
+browser.get("https://fake-shop.com/book2.html")
+
+# adding unit to cart
+add_button = browser.find_element_by_css_selector(".add")
+add_button.click()
+
+# opening a cart
+browser.get("https://fake-shop.com/basket.html")
+
+# searching a list of all units (goods)
+goods = browser.find_elements_by_css_selector(".good")
+
+# checking for a ammount of units = 2
+assert len(goods) == 2
+
+browser.quit()
