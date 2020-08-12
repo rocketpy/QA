fd37ca3dd3d89c623ff678875a996ad02b90b449
rocketpy <dimitr.ushakov@gmail.com>
2020-08-12 15:50:55 +0300

    added main code

--- stepik/find_elems.py
+++ stepik/find_elems.py
@@ -1,1 +1,25 @@
+import time
+from selenium import webdriver
+ 
 
+LINK = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(LINK)
+
+    input1 = browser.find_element_by_tag_name(value1)
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name(value2)
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name(value3)
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id(value4)
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+
+    browser.quit()
