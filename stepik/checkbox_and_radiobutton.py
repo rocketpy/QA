9370007f99b74d532812cc0862dd669f93a71141
rocketpy <dimitr.ushakov@gmail.com>
2020-08-13 17:06:53 +0300

    updated

--- stepik/checkbox_and_radiobutton.py
+++ stepik/checkbox_and_radiobutton.py
@@ -1,1 +1,27 @@
+#  <input type="radio" name="language" value="python" checked>
+elem = browser.find_element_by_css_selector("[value='python']")
+elem.click()
+
+"""
+<input type="checkbox">  # we can make choice for many elems 
+<input type="radio">  # we can make choice only for one elem 
+
+#  checked
+<input type="checkbox" checked>
+<input type="radio" checked>
+
+<input type="radio" name="language" value="python" checked>
+<input type="radio" name="language" value="selenium">
+"""
+
+# using tag <label> , this tag make text a cklickable !!!
+option1 = browser.find_element_by_css_selector("[for='python']")
+option1.click()
+
+"""
+<div>
+  <input type="radio" id="python" name="language" checked>
+  <label for="python">Python</label>
+</div>
+"""
 
