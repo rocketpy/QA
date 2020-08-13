ee80d03c613792014f17ea0dbf6f29a2980ba516
rocketpy <dimitr.ushakov@gmail.com>
2020-08-13 21:06:22 +0300

    updated

--- stepik/lists_or_dropdown_lists.py
+++ stepik/lists_or_dropdown_lists.py
@@ -1,1 +1,20 @@
+from selenium import webdriver
+# from selenium.webdriver.support.ui import Select
 
+
+LINK = ""
+
+browser = webdriver.Chrome()
+browser.get(LINK)
+
+browser.find_element_by_tag_name("select").click()
+browser.find_element_by_css_selector("option:nth-child(2)").click()  
+#  or
+browser.find_element_by_css_selector("[value='1']").click()
+
+# or by Select
+select = Select(browser.find_element_by_tag_name("select"))
+select.select_by_value("1") 
+
+
+browser.quit()
