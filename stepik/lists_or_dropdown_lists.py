8ad97898883dffed714bf7d933cffc993c35486a
rocketpy <dimitr.ushakov@gmail.com>
2020-08-13 21:18:46 +0300

    updated

--- stepik/lists_or_dropdown_lists.py
+++ stepik/lists_or_dropdown_lists.py
@@ -12,9 +12,15 @@ browser.find_element_by_css_selector("option:nth-child(2)").click()
 #  or
 browser.find_element_by_css_selector("[value='1']").click()
 
-# or by Select
+# or by Select , it's a best practice !
 select = Select(browser.find_element_by_tag_name("select"))
 select.select_by_value("1") 
+
+# Select by visible text
+select.select_by_visible_text("text")
+
+#  Select by index
+elect.select_by_index(index)  # first index is 0
 
 
 browser.quit()
