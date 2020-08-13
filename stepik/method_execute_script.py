78eb372cd57cf9580ef199bd2b29c72b54d914ad
rocketpy <dimitr.ushakov@gmail.com>
2020-08-13 22:10:24 +0300

    added a main code

--- stepik/method_execute_script.py
+++ stepik/method_execute_script.py
@@ -1,1 +1,12 @@
+from selenium import webdriver
 
+
+browser = webdriver.Chrome()
+
+# make a simple example with alert on web page
+browser.execute_script("alert('Robots at work');")
+#  or
+browser.execute_script("document.title='Script executing';")
+
+# two and more commands in one
+browser.execute_script("document.title='Script executing';alert('Robots at work');")  # change title and call alert
