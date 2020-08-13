3814ed84f7df064662d9ee01a9f8e70a0b890a95
rocketpy <dimitr.ushakov@gmail.com>
2020-08-13 18:52:22 +0300

    updated

--- stepik/checkbox_and_radiobutton.py
+++ stepik/checkbox_and_radiobutton.py
@@ -25,3 +25,23 @@ option1.click()
 </div>
 """
 
+# using method get_attribute()
+"""
+<input class="check-input" type="radio" name="ruler" id="peopleRule" value="people" checked>
+
+people_radio = browser.find_element_by_id("peopleRule")
+people_checked = people_radio.get_attribute("checked")
+print("value of people radio: ", people_checked)
+assert people_checked is not None, "People radio is not selected by default"
+"""
+#  assert people_checked == "true", "People radio is not selected by default"
+
+"""
+robots_radio = browser.find_element_by_id("robotsRule")
+robots_checked = robots_radio.get_attribute("checked")
+assert robots_checked is None
+"""
+
+# disabled attribute for button "Submit"
+# <button type="submit" class="btn btn-default" disabled>Submit</button>
+
