75d9b814d9f0da36047d443b6da00d4914fc62fe
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-13 17:41:59 +0300

    modified

--- stepik/captcha_for_robot.py
+++ stepik/captcha_for_robot.py
@@ -18,19 +18,19 @@ try:
     x = x_element.text
     y = calc(x)
 
-    input_answer = browser.find_element_by_id("answer")
+    input_answer = browser.find_element_by_class_name("form-control")
     input_answer.send_keys(y)
     
     button_i_am_robot = browser.find_element_by_class_name("form-check-input")
     button_i_am_robot.click()
     
-    button_robots_rule = browser.find_element_by_css_selector("[name='ruler']")
+    button_robots_rule = browser.find_element_by_css_selector("[value='robots']")
     button_robots_rule.click()
 
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
 finally:
-    time.sleep(3)
+    time.sleep(5)
 
     browser.quit()
