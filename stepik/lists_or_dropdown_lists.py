c7352a12589d1223e1d3d6ae0537e5783b9a5177
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-13 21:42:43 +0300

    updated

--- stepik/lists_or_dropdown_lists.py
+++ stepik/lists_or_dropdown_lists.py
@@ -1,12 +1,13 @@
+import time
 from selenium import webdriver
-# from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select
 
 
-LINK = ""
+LINK = "http://suninjuly.github.io/selects1.html"
 
 browser = webdriver.Chrome()
 browser.get(LINK)
-
+"""
 browser.find_element_by_tag_name("select").click()
 browser.find_element_by_css_selector("option:nth-child(2)").click()  
 #  or
@@ -20,7 +21,18 @@ select.select_by_value("1")
 select.select_by_visible_text("text")
 
 #  Select by index
-elect.select_by_index(index)  # first index is 0
+select.select_by_index(index)  # first index is 0
+"""
 
+try:
+    first_num = browser.find_element_by_id("num1").text
+    second_num = browser.find_element_by_id("num2").text
+    result = int(first_num) + int(second_num)
+    select = Select(browser.find_element_by_id("dropdown"))
+    select.select_by_value(str(result))
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
 
-browser.quit()
+finally:
+    time.sleep(5)
+    browser.quit()
