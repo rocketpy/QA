399df76186c0fb7b10480335b8e41d0b96ce4fb7
rocketpy <dimitr.ushakov@gmail.com>
2020-08-14 21:53:50 +0300

    updated

--- stepik/browser_windows.py
+++ stepik/browser_windows.py
@@ -1,1 +1,11 @@
+#  get a current page name (number)
+current_window = browser.current_window_handle
 
+# go to window_name:
+browser.switch_to.window(window_name)
+
+# name a second opened window
+new_window = browser.window_handles[1]
+
+# first window name
+first_window = browser.window_handles[0]
