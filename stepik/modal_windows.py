1a22bf806b598c803c104591926a6653717ed453
rocketpy <dimitr.ushakov@gmail.com>
2020-08-14 21:35:09 +0300

    updated

--- stepik/modal_windows.py
+++ stepik/modal_windows.py
@@ -29,3 +29,28 @@ prompt.accept()
 
 LINK = "http://suninjuly.github.io/alert_accept.html"
 browser = webdriver.Chrome()
+
+try:
+    browser.get(LINK)
+    
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    
+    first_button = browser.find_element_by_css_selector("button.btn")
+    first_button.click()
+    
+    alert_window = browser.switch_to.alert
+    alert.accept()
+    
+    x_element = browser.find_element_by_class_name("nowrap")
+    x = x_element.text
+    y = calc(x)
+    
+    input_answer = browser.find_element_by_class_name("form-control")
+    input_answer.send_keys(y)
+    
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+finally:
+    time.sleep(7)
+    browser.quit()
