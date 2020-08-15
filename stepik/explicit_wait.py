39cff8e622318a1b9ce9fd593e1c7a4d5f60137b
rocketpy <dimitr.ushakov@gmail.com>
2020-08-15 15:08:15 +0300

    updated info

--- stepik/explicit_wait.py
+++ stepik/explicit_wait.py
@@ -4,11 +4,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 
+"""
+options = webdriver.ChromeOptions()
+options.add_experimental_option('w3c', False)  # use this if something not working ...
+browser = webdriver.Chrome(options=options)
+"""
+
 browser = webdriver.Chrome()
 
 browser.get("http://suninjuly.github.io/wait2.html")
 
 button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "verify")))  #  checking a button up to 5 seconds, until button be a clickable !!! 
+#  button = browser.find_element_by_css_selector("button:not([disabled])")
 
 button.click()
 message = browser.find_element_by_id("verify_message")
