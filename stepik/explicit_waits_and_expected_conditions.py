558fe656fb3484b677ee68263a8abb7d06c290f8
rocketpy <dimitr.ushakov@gmail.com>
2020-08-15 17:09:27 +0300

    updated main code

--- stepik/explicit_waits_and_expected_conditions.py
+++ stepik/explicit_waits_and_expected_conditions.py
@@ -5,12 +5,37 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 
+LINK = "http://suninjuly.github.io/explicit_wait2.html"
 browser = webdriver.Chrome()
 
-browser.get("http://suninjuly.github.io/wait2.html")
+try:
+    browser.get(LINK)
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+      
+    price = WebDriverWait(browser, 13).until(EC.text_to_be_present_in_element((By.XPATH,"//*[@id="price"]"),"$100"))
+    
+    button = browser.find_element_by_css_selector("button.btn.btn-primary")
+    button.click()
+
+    x_element = browser.find_element_by_xpath('//*[@id="treasure"]')
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    input_answer = browser.find_element_by_id("answer")
+    input_answer.send_keys(y)
+
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(5)
+
+    browser.quit()
+
 
 button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "verify")))
 button.click()
-
-message = browser.find_element_by_id("verify_message")
 
