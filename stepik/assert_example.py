dda7d2ca46c61e97d417b520d0d5273b64c0c13b
rocketpy <dimitr.ushakov@gmail.com>
2020-08-15 14:44:41 +0300

    updated

--- stepik/assert_example.py
+++ stepik/assert_example.py
@@ -1,9 +1,11 @@
+import time
 from selenium import webdriver
 
 
 browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/wait1.html")
 
+time.sleep(2)  # waiting 2 seconds for download a button
 button = browser.find_element_by_id("verify")
 button.click()
 
