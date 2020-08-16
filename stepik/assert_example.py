b35672b3cdbeae790b9bb947fe8a71af4b22bc77
rocketpy <dimitr.ushakov@gmail.com>
2020-08-16 15:51:04 +0300

    updated

--- stepik/assert_example.py
+++ stepik/assert_example.py
@@ -8,7 +8,11 @@ browser.get("http://suninjuly.github.io/wait1.html")
 time.sleep(2)  # waiting 2 seconds for download a button
 button = browser.find_element_by_id("verify")
 button.click()
-
 message = browser.find_element_by_id("verify_message")
 
 assert "successful" in message.text
+
+
+#  or  , compound error messages
+def test_input_text(expected_result, actual_result):
+        assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}" 
