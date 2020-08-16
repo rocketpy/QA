48e7bfd4e69e945960bbfa02066e2816fcf3dd8d
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2020-08-16 23:54:46 +0300

    updated

--- stepik/unittest_example.py
+++ stepik/unittest_example.py
@@ -28,31 +28,24 @@ browser = webdriver.Chrome()
 class TestForms(unittest.TestCase):
     def test_form_1(self):
         browser.get(LINK_1)
-        input_1 = browser.find_element_by_class_name("form-control.first")
-        input_1.send_keys("John")
-        input_2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input")
-        input_2.send_keys("Doe")
-        input_3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input")
-        input_3.send_keys("abc123@gmail.com")
-        button_1 = browser.find_element_by_css_selector("button.btn")
-        button_1.click()
+        input_1 = browser.find_element_by_class_name("form-control.first").send_keys("John")
+        input_2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input").send_keys("Doe")
+        input_3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input").send_keys("abc123@gmail.com")
+        button_1 = browser.find_element_by_css_selector("button.btn").click
+
         time.sleep(3)
         # browser.quit()
-        self.assertEqual(input_2, input_2, "NoSuchElementException ")
+        assert input_2 == input_2, "NoSuchElementException "
 
     def test_form_2(self):
         browser.get(LINK_2)
-        input1 = browser.find_element_by_class_name("form-control.first")
-        input1.send_keys("John")
-        input2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input")
-        input2.send_keys("Doe")
-        input3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input")
-        input3.send_keys("abc123@gmail.com")
-        button = browser.find_element_by_css_selector("button.btn")
-        button.click()
+        input1 = browser.find_element_by_class_name("form-control.first").send_keys("John")
+        input2 = browser.find_element_by_css_selector("div.first_block > div.form-group.second_class > input").send_keys("Doe")
+        input3 = browser.find_element_by_xpath("/html/body/div/form/div[1]/div[3]/input").send_keys("abc123@gmail.com")
+        button = browser.find_element_by_css_selector("button.btn").click()
         time.sleep(3)
         browser.quit()
-        self.assertEqual(input2, input2, "NoSuchElementException")
+        assert input2 == input2, "NoSuchElementException"
         
         
 if __name__ == "__main__":
