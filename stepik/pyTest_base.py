4777ea750f90451ae959481723d3608338caf31c
rocketpy <dimitr.ushakov@gmail.com>
2020-08-16 22:48:08 +0300

    updated info

--- stepik/pyTest_base.py
+++ stepik/pyTest_base.py
@@ -1,1 +1,5 @@
+#  install to venv:
+# > selenium_env\Scripts\activate.bat 
+# (selenium_env) С:\Users\user\environments>  pip install pytest==5.1.1
 
+# to run PyTest flle:  pytest test_abs_project.py
