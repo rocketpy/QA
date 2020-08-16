e76e38baf1f85f933289d9a370bc0b9784d848d3
rocketpy <dimitr.ushakov@gmail.com>
2020-08-16 16:12:05 +0300

    updated

--- stepik/assert_example.py
+++ stepik/assert_example.py
@@ -16,3 +16,8 @@ assert "successful" in message.text
 #  or  , compound error messages
 def test_input_text(expected_result, actual_result):
         assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}" 
+        
+#  compound error messages and substring search
+def test_substring(full_string, substring):
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'" 
+        
