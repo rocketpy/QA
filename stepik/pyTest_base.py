8ac7de81d1a32c672c990d245304b083eb58ea80
rocketpy <dimitr.ushakov@gmail.com>
2020-08-17 18:40:03 +0300

    updated info

--- stepik/pyTest_base.py
+++ stepik/pyTest_base.py
@@ -7,11 +7,14 @@
 
 # a simple example , file with name test_abs.py
 def test_abs_1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+    assert abs(-42) == 42, "Should be absolute value of a number"  #  return True
 
 def test_abs_2():
-    assert abs(-42) == -42, "Should be absolute value of a number"
-
-# for run tests in cosole:  pytest test_abs.py 
+    assert abs(-42) == -42, "Should be absolute value of a number"   # return False
+    
+#  for run tests in cosole:  pytest test_abs.py 
 
 #  and try add this to command:  -v --tb=line
+
+#  we can use any construction: == , != , e.t.c. 
+#  and like this:  assert user_is_authorised(), "User is guest"
