b4b8f99bbb25b64303ec5e815ccde71021a61b05
rocketpy <dimitr.ushakov@gmail.com>
2020-08-17 18:20:10 +0300

    updated

--- stepik/pyTest_base.py
+++ stepik/pyTest_base.py
@@ -3,3 +3,15 @@
 # (selenium_env) С:\Users\user\environments>  pip install pytest==5.1.1
 
 # to run PyTest flle:  pytest test_abs_project.py
+
+
+# a simple example , file with name test_abs.py
+def test_abs_1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs_2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+# for run tests in cosole:  pytest test_abs.py 
+
+#  and try add this to command:  -v --tb=line
