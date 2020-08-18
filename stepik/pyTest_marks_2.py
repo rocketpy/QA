b0ddc9ec137fec665102d552a6da52ad0f9cc75e
rocketpy <dimitr.ushakov@gmail.com>
2020-08-18 17:28:30 +0300

    adding some commands

--- stepik/pyTest_marks_2.py
+++ stepik/pyTest_marks_2.py
@@ -1,1 +1,4 @@
+#  inversion - it means , run tests NOT for SMOKE use command:  pytest -s -v -m "not smoke" test_example.py
+
+#  for united tests need use:  pytest -s -v -m "smoke or regression" test_example.py
 
