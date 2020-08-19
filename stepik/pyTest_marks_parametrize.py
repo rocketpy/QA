c09f56049177142bf305daf7d808bbb356cb5843
rocketpy <dimitr.ushakov@gmail.com>
2020-08-19 16:20:01 +0300

    updated info

--- stepik/pyTest_marks_parametrize.py
+++ stepik/pyTest_marks_parametrize.py
@@ -18,4 +18,17 @@ def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+    
+    
+#  using parametrize() for classes
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+        # этот тест запустится 2 раза
+
+    def test_guest_should_see_navbar_element(self, browser, language):
+        # this test must be run twice
 
