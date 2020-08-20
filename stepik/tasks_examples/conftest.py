e98284aab2027e4124c57c3a01da6cec4dccf295
rocketpy <dimitr.ushakov@gmail.com>
2020-08-20 16:55:40 +0300

    added a main code

--- stepik/tasks_examples/conftest.py
+++ stepik/tasks_examples/conftest.py
@@ -1,1 +1,13 @@
+#  here we put our fixture
 
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
