84e52962b939ce99710d35aca2ac5ab2f6bacb37
rocketpy <dimitr.ushakov@gmail.com>
2020-08-20 16:58:48 +0300

    updated info

--- stepik/tasks_examples/conftest.py
+++ stepik/tasks_examples/conftest.py
@@ -1,4 +1,5 @@
 #  here we put our fixture
+#  Можно создавать дополнительные файлы conftest.py в других директориях, но тогда настройки в этих файлах будут применяться только к тестам в под-директориях.
 
 import pytest
 from selenium import webdriver
@@ -11,3 +12,13 @@ def browser():
     yield browser
     print("\nquit browser..")
     browser.quit()
+    
+#  now we can use fixture in ANY TEST !!!
+# in file  test_conftest.py , like example:
+"""
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+"""
