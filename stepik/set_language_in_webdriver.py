395a36a26aa2eb46c23ec57fe4a49144d07f9f48
rocketpy <dimitr.ushakov@gmail.com>
2020-08-20 21:54:41 +0300

    added a main code

--- stepik/set_language_in_webdriver.py
+++ stepik/set_language_in_webdriver.py
@@ -1,1 +1,12 @@
+from selenium.webdriver.chrome.options import Options
+#  Options using for: set language, set proxy , different versions of browsers !!!  
 
+#  set language for Chrome
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+# set language for Firefox
+fp = webdriver.FirefoxProfile()
+fp.set_preference("intl.accept_languages", user_language)
+browser = webdriver.Firefox(firefox_profile=fp)
