58dd7d7c0548cb4dd30354ac702f9429d91bfb53
rocketpy <dimitr.ushakov@gmail.com>
2020-08-20 17:00:01 +0300

    updated info

--- stepik/tasks_examples/conftest.py
+++ stepik/tasks_examples/conftest.py
@@ -22,3 +22,15 @@ def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
 """
+
+#  structure must be like:
+"""
+selenium_course_solutions/
+├── section3
+│   └── conftest.py
+│   └── test_languages.py
+├── section4 
+│   └── conftest.py
+│   └── test_main_page.py
+
+"""
