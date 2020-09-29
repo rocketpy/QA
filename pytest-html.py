276bf21d72040e2222d71ce9ec4d0b038dfc994d
rocketpy <dimitr.ushakov@gmail.com>
2020-09-29 20:05:24 +0300

    updated

--- pytest-html.py
+++ pytest-html.py
@@ -1,1 +1,6 @@
+#  pytest-html is a plugin for pytest that generates a HTML report for the test results
 
+#  pip install pytest-html
+
+#  Docs:  https://pypi.org/project/pytest-html/
+#  Github:  https://github.com/pytest-dev/pytest-html
