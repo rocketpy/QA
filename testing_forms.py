ef4c1b343103a7708f07e24d58bd28ad7aef84ac
rocketpy <dimitr.ushakov@gmail.com>
2020-10-02 23:02:22 +0300

    added code

--- testing_forms.py
+++ testing_forms.py
@@ -1,1 +1,13 @@
+from selenium import webdriver
+
+
+#PATH = "C:\Program Files\chromedriver.exe" 
+#driver = webdriver.Chrome(PATH) 
+
+PROXY = "12.345.678.910:8080"
+options = WebDriverWait.ChromeOptions()
+options.add_argument('--proxy-server=%s' % PROXY)
+chrome = webdriver.Chrome(chrome_options=options)
+
+driver.get("https://...")
 
