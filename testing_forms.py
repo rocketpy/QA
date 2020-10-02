adbd2c5b74ec14245e4a3cf808236f55ce7d695f
rocketpy <dimitr.ushakov@gmail.com>
2020-10-02 23:05:07 +0300

    updated

--- testing_forms.py
+++ testing_forms.py
@@ -9,5 +9,17 @@ options = WebDriverWait.ChromeOptions()
 options.add_argument('--proxy-server=%s' % PROXY)
 chrome = webdriver.Chrome(chrome_options=options)
 
-driver.get("https://...")
+TC_1 = ''
+TC_2 = ''
+TC_3 = ''
+TC_4 = ''
+TC_5 = ''
+TC_6 = ''
+TC_7 = ''
+TC_8 = ''
+TC_9 = ''
+TC_10 = ''
+TC_11 = ''
+TC_12 = ''
 
+driver.get("https://...")
