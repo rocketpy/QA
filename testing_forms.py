79af6e1a1f40e8e01b2801e6e9254e678835dd52
rocketpy <dimitr.ushakov@gmail.com>
2020-10-03 15:14:17 +0300

    updated

--- testing_forms.py
+++ testing_forms.py
@@ -15,7 +15,7 @@ chrome = webdriver.Chrome(chrome_options=options)
 # EMAIL = "vasya_pupkin@gmail.com"
 # LOGIN = ""
 # PASSWORD = ""
-# CINFIRM_PASSWORD = PASSWORD
+# CONFIRM_PASSWORD = PASSWORD
 
 #  test cases for fields
 TC_1 = ''
@@ -30,10 +30,11 @@ TC_9 = 'ABCDEFGH'
 TC_10 = 'vasya_pupkin@@@gmail.com'
 TC_11 = 'a'
 TC_12 = '1'
-TC_13 = '<script>document.body.style.backgroundColor = "#000";</script>'
-TC_14 = '$detail_id=$_GET['detail'];'
-TC_15 = 'http://www.site.com/page.php?var=<script>alert('xss');</script>'
-TC_16 = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11111111111111111111111111111111111111111111111111
+TC_13 = '               '
+TC_14 = '<script>document.body.style.backgroundColor = "#000";</script>'
+TC_15 = '$detail_id=$_GET['detail'];'
+TC_16 = 'http://www.site.com/page.php?var=<script>alert('xss');</script>'
+TC_17 = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11111111111111111111111111111111111111111111111111
          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11111111111111111111111111111111111111111111111111
          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11111111111111111111111111111111111111111111111111
          xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx11111111111111111111111111111111111111111111111111
@@ -54,11 +55,11 @@ email_field.send_keys("Abcdefgh")
 password_field = driver.find_element_by_xpath('')  # input field 
 password_field.send_keys("Abcdefgh")
 
-submit_button = driver.find_element_by_xpath('')  # submit button
-submit_button.click()
-
 confirm_password_field = driver.find_element_by_xpath('')  # input field 
 confirm_password_field.send_keys("Abcdefgh")
+
+submit_button = driver.find_element_by_xpath('')  # submit button
+submit_button.click()
 
 # driver.find_element_by_xpath("").click()
 
