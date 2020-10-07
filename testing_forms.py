6f5c5e4410b02610fd0f6a5c0c03fef1f33d6946
rocketpy <dimitr.ushakov@gmail.com>
2020-10-07 22:05:29 +0300

    updated

--- testing_forms.py
+++ testing_forms.py
@@ -6,6 +6,15 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 #PATH = "C:\Program Files\chromedriver.exe" 
 #driver = webdriver.Chrome(PATH) 
 
+"""
+prox = Proxy()
+prox.proxy_type = ProxyType.MANUAL
+
+prox.http_proxy = “0.0.0.0:00000”
+prox.socks_proxy = “0.0.0.0:00000”
+prox.ssl_proxy = “0.0.0.0:00000”
+"""
+
 PROXY = "12.345.678.910:8080"  #  for HTTP
 PROXY_S = "12.345.678.910:443"  #  for HTTPS
 options = WebDriverWait.ChromeOptions()
