d9a6915db123e2847ea9f270c9b2f1b19f98f22b
rocketpy <dimitr.ushakov@gmail.com>
2020-10-10 23:12:01 +0300

    updated

--- scripting_for_testers.py
+++ scripting_for_testers.py
@@ -2,6 +2,7 @@
 import requests
 
 
+#  API calls 
 url = 'http://jsonplaceholder.typicode.com/photos'
 response = requests.get(url)  # API call
 # print(response.json())
@@ -9,7 +10,21 @@ json_payload = {'albumID': 1, 'title': 'test', 'url': 'test.com'}
 resp_post = requests.post(url, json=json_payload)
 resp_post.json()
 
-
 url_2 = 'http://jsonplaceholder.typicode.com/photos/100'
 resp_post = requests.put(url, json=json_payload)
 resp_post.json()
+
+resp_post = requests.delete(url, json=json_payload)
+resp_post.json()
+
+#  API authentication
+import requests
+
+
+url = 'https://api.github.com/user'
+response = requests.get(url)
+response.json()
+
+response = requests.get(url, auth=('test_name', 'password'))
+response.json()
+
