fd447ac3c0c845bddfee0d09caa8ac8aec624052
rocketpy <dimitr.ushakov@gmail.com>
2020-10-31 16:27:02 +0200

    updated

--- load_testing_API.py
+++ load_testing_API.py
@@ -1,1 +1,10 @@
+"""
+#  for uniques user use module Faker
 
+def registration:
+    URL = "ip"
+    PARAMS = {'name':'test','password':'test1','primary_email':'test667@gmail.com','primary_mobile_number':'9999999999','country_abbrev':'US'} 
+    r = requests.post(url = URL,params = PARAMS,auth=HTTPDigestAuth('user', 'pass')) 
+    response = r.text 
+    print response
+"""
