cb6c86b0d4f7914fe26ceac76ece9a8791476cb9
rocketpy <dimitr.ushakov@gmail.com>
2020-11-09 21:51:13 +0200

    added a second class

--- login_with_locust.py
+++ login_with_locust.py
@@ -5,5 +5,15 @@ class LoginWithUniqueUser(TaskSet):
     @task
     def login(self):
         self.client.post("/login", {
-            'email': 'User1@gmail.com', 'password': '123456'
+            'email': 'User1@gmail.com', 'password': '1234567890'
         })
+
+        
+class LoginWithUniqueUserTest(HttpLocust):
+    task_set = LoginWithUniqueUsersSteps
+    host = "http://"
+    sock = None
+
+    def __init__(self):
+        super(LoginWithUniqueUserTest, self).__init__()        
+        
