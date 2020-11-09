2bc2cb9b24586147afa60c516507382ee0b5ac0c
rocketpy <dimitr.ushakov@gmail.com>
2020-11-09 21:44:59 +0200

    added a new class

--- login_with_locust.py
+++ login_with_locust.py
@@ -1,1 +1,9 @@
+from locust import HttpLocust, TaskSet, task
 
+
+class LoginWithUniqueUser(TaskSet):
+    @task
+    def login(self):
+        self.client.post("/login", {
+            'email': 'User1@gmail.com', 'password': '123456'
+        })
