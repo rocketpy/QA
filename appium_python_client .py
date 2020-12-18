af48a5c20d5b98d81dfa54353751ad19641deb16
rocketpy <dimitr.ushakov@gmail.com>
2020-12-18 22:12:23 +0200

    updated

--- appium_python_client .py
+++ appium_python_client .py
@@ -4,4 +4,17 @@
 
 #  pip install Appium-Python-Client
 
+# Android environment
+import unittest
+from appium import webdriver
 
+
+desired_caps = dict(platformName = 'Android'
+                    platformVersion = '10'
+                    automationName = 'uiautomator2'
+                    deviceName = 'Android Emulator'
+                    app = PATH('../../../apps/selendroid-test-app.apk')
+                    )
+self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+el = self.driver.find_element_by_accessibility_id('item')
+el.click()
