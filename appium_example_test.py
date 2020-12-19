3b7c7bdbcdbdcbf3af731c3b5abe37dea3f29b2f
rocketpy <dimitr.ushakov@gmail.com>
2020-12-19 22:14:47 +0200

    updated

--- appium_example_test.py
+++ appium_example_test.py
@@ -1,1 +1,8 @@
+from appium import webdriver
 
+
+desired_caps = {'platformName': 'iOS',
+                'platformVersion': '11.1',
+                'deviceName': 'iPhone SE',
+                'app': '/Users/...app'
+                }
