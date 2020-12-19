b009302c7b0e8a6abed7ff4a7f9c1345ff10fec2
rocketpy <dimitr.ushakov@gmail.com>
2020-12-19 22:05:58 +0200

    updated

--- appium_python_client .py
+++ appium_python_client .py
@@ -35,3 +35,17 @@ desired_caps = dict(platformName = 'iOS'
 self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 el = self.driver.find_element_by_accessibility_id('item')
 el.click()
+
+
+import unittest
+from appium import webdriver
+
+
+desired_caps = dict(platformName = 'iOS'
+                    platformVersion = '13.4'
+                    automationName = 'xcuitest'
+                    deviceName = 'iPhone Simulator'
+                    app = PATH('../../apps/UICatalog.app.zip')
+                    )
+
+self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps, direct_connection=True)
