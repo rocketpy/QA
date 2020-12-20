c3844acf26a4cb66864c6461bb7c1caa8a34a9e8
rocketpy <dimitr.ushakov@gmail.com>
2020-12-20 18:40:53 +0200

    updated

--- appium_example_test.py
+++ appium_example_test.py
@@ -6,3 +6,13 @@ desired_caps = {'platformName': 'iOS',
                 'deviceName': 'iPhone SE',
                 'app': '/Users/...app'
                 }
+
+def test_sum_two_integers():
+    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+    driver.find_element_by_name('3').click()
+    driver.find_element_by_name('+').click()
+    driver.find_element_by_name('2').click()
+    driver.find_element_by_name('=').click()
+    numbers_field = driver.find_element_by_xpath('//TypeStaticText')
+    assert int(numbers_field.text) == 5
+    driver.quit()
