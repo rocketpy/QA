b74b161f9f915c9f3855736ce7cf997f63a17f88
rocketpy <dimitr.ushakov@gmail.com>
2020-12-21 19:19:54 +0200

    updated

--- API_REST_testing.py
+++ API_REST_testing.py
@@ -2,3 +2,27 @@
 #  pip install -U requests Flask pytest pytest-html
 
 
+
+#  some example from Postman (create a Postman API request test and take code )
+"""
+import requests
+
+
+url = "http://httpbin.org/post"
+payload = "{\n    \"key1\": 1,\n    \"key2\": \"value2\"\n}"
+headers = {
+    'Content-Type': "",
+    'User-Agent': "Postman",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "",
+    'Host': "httpbin.org",
+    'accept-encoding': "",
+    'content-length': "",
+    'Connection': "",
+    'cache-control': ""
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+# print(response.text)
+"""
