13e0c6e0d7b93c07696679883456ff5b659cbf53
rocketpy <dimitr.ushakov@gmail.com>
2020-12-21 19:27:46 +0200

    updated

--- API_REST_testing.py
+++ API_REST_testing.py
@@ -2,25 +2,30 @@
 #  pip install -U requests Flask pytest pytest-html
 
 
-import requests
 import json
+import requests
 
+# for run this test with pytest use: pytest  
 
-def test_post_headers_body_json():
+def test_post_headers_body_to_json():
     url = 'https://httpbin.org/post'
     headers = {'Content-Type': 'application/json'} 
     payload = {'key1': 1, 'key2': 'value2'}
     # need convert dict to json string by json.dumps() for body data. 
     resp = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))    
-
-
+    assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body['url'] == url
+    # print(resp.text)
+    
+    
 #  some example from Postman (create a Postman API request test and take code )
 """
 import requests
 
 
 url = "http://httpbin.org/post"
-payload = "{\n    \"key1\": 1,\n    \"key2\": \"value2\"\n}"
+payload = "{...}"
 headers = {
     'Content-Type': "",
     'User-Agent': "Postman",
