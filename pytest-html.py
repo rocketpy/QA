12d55381b0ef7410ff13765cf4cff4495f2d53cb
rocketpy <dimitr.ushakov@gmail.com>
2020-12-24 20:24:46 +0200

    Update pytest-html.py

--- pytest-html.py
+++ pytest-html.py
@@ -4,3 +4,11 @@
 
 #  Docs:  https://pypi.org/project/pytest-html/
 #  Github:  https://github.com/pytest-dev/pytest-html
+
+#  requirements:
+"""
+python3 ( Linux: apt-get install python3 )
+pip3 ( Linux: apt-get install python3-pip ) 
+pytest ( Linux: pip3 install pytest )
+pytest-html ( Linux: pip3 install pytest-html ) 
+"""
