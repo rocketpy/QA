492fbf3557f0ff724ea0e9c6caf34f41ca030bbe
rocketpy <dimitr.ushakov@gmail.com>
2020-12-25 23:23:31 +0200

    Update POM.py

--- POM.py
+++ POM.py
@@ -2,3 +2,4 @@
 #  1 - Creating a Class for every page
 #  2 - Creating a methods for every action on the page
 #  3 - Before testing a page , check current page , login_page = current_page (login page)
+#  4 - Creating a new file for fixtures - conftest.py
