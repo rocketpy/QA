2a8784957e8e4e352a91c42acc89c76b95b50ba9
rocketpy <dimitr.ushakov@gmail.com>
2020-12-25 20:05:51 +0200

    Update POM.py

--- POM.py
+++ POM.py
@@ -1,1 +1,4 @@
-
+#  Hints:
+#  1 - Creating a Class for every page
+#  2 - Creating a methods for every action on the page
+#  3 - Before testing a page , check current page , login_page = current_page (login page)
