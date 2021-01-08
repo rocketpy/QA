c09402e3fcb0879b2d6b598b0a325ebd0c735b1b
rocketpy <dimitr.ushakov@gmail.com>
2021-01-08 22:56:40 +0200

    Update Login_cookies_Firefox.py

--- Login_cookies_Firefox.py
+++ Login_cookies_Firefox.py
@@ -1,1 +1,12 @@
+import time
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+
+options = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", "...")
+driver = webdriver.Firefox(executable_path="...", options=options)
+
+LOGIN = ""
+PASSWORD = ""
