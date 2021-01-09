80ac7dd841cd0210b3bb8695bb367bfa568dc105
rocketpy <dimitr.ushakov@gmail.com>
2021-01-09 19:08:07 +0200

    Update Login_cookies_Firefox.py

--- Login_cookies_Firefox.py
+++ Login_cookies_Firefox.py
@@ -10,3 +10,26 @@ driver = webdriver.Firefox(executable_path="...", options=options)
 
 LOGIN = ""
 PASSWORD = ""
+
+try:
+    # driver.get("https://instagram.com/")
+    # time.sleep(5)
+    #
+    # username_input = driver.find_element_by_name("username")
+    # username_input.clear()
+    # username_input.send_keys(instagram_login)
+    # time.sleep(5)
+    #
+    # password_input = driver.find_element_by_name("password")
+    # password_input.clear()
+    # password_input.send_keys(instagram_password)
+    # time.sleep(5)
+    #
+    # password_input.send_keys(Keys.ENTER)
+    # time.sleep(10)
+    #
+    # # # cookies
+    # pickle.dump(driver.get_cookies(), open(f"{instagram_login}_cookies", "wb"))
+
+    driver.get("https://instagram.com/")
+    time.sleep(5)
