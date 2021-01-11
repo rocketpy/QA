dcc18c3b5d3b56099f88f01bd8d540095cb027b5
rocketpy <dimitr.ushakov@gmail.com>
2021-01-11 23:04:35 +0200

    Update postpy2_postman.py

--- postpy2_postman.py
+++ postpy2_postman.py
@@ -1,1 +1,23 @@
+#  Postpy2 is a library for Postman that run Postman's collections
 
+# PyPi: https://pypi.org/project/postpy2/
+# Docs: https://github.com/matkapi/postpy2
+
+# pip install Postpy2
+
+from Postpy2.core import Postpy2
+
+
+runner = Postpy2('/path/to/collection/Postman echo.postman_collection')
+
+response = runner.RequestMethods.get_request()
+
+print(response.json())
+print(response.status_code)
+
+#  load enviromental variables from postman enviroment files
+pp.environments.load('environments/test.postman_environment.json')
+
+# assign values to environment variables in runtime
+runner.environments.update({'BASE_URL': 'http://127.0.0.1:5000'})
+runner.environments.update({'PASSWORD': 'test', 'EMAIL': 'you@email.com'})
