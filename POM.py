319d8a8c4f78d7a2db6853f502dbbe7723152e9c
rocketpy <dimitr.ushakov@gmail.com>
2021-01-19 20:15:41 +0200

    Update POM.py

--- POM.py
+++ POM.py
@@ -1,5 +1,8 @@
 #  Hints:
-#  1 - Creating a Class for every page
-#  2 - Creating a methods for every action on the page
-#  3 - Before testing a page , check current page , login_page = current_page (login page)
-#  4 - Creating a new file for fixtures - conftest.py
+#  1 - Creating a file with base Class for all pages (including methods).
+#  2 - Creating a files with Class for every page.
+#  3 - Creating a methods for every action on the page.
+#  4 - Before testing a page , check current page , login_page = current_page.
+#  5 - Creating a new file for fixtures - conftest.py
+#  6 - Creating file with Locators for all pages.
+#  7 - Creating a files (test_page_name.py) for every page.
