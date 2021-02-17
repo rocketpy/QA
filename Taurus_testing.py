fd478a7152e345bedaaf8abadfdfd0e0585f5615
rocketpy <dimitr.ushakov@gmail.com>
2021-02-17 21:47:18 +0200

    Update Taurus_testing.py

--- Taurus_testing.py
+++ Taurus_testing.py
@@ -6,7 +6,28 @@
 
 #  pip install bzt
 
+#  Get started:
+#  Create a file named test.yml with following contents:
+"""
+---
+execution:
+- concurrency: 10
+  ramp-up: 1m
+  hold-for: 1m30s
+  scenario: simple
+  
+scenarios:
+  simple:
+    think-time: 0.75
+    requests:
+    - http://blazedemo.com/
+    - http://blazedemo.com/vacation.html
+"""
+#  Then run bzt test.yml
+
+
 #  Example for Locust , use locust.py
+#  pip install locustio
 from locust import HttpUser, TaskSet, task, between
  
   
@@ -28,5 +49,22 @@ class WebsiteTasks(TaskSet):
 class WebsiteUser(HttpUser):
     tasks = [WebsiteTasks]
     wait_time = between(0.100, 1.500)
+   
+   
+#  example config that uses existing locust file:   YAML
+"""
+execution:
+- executor: locust
+  concurrency: 10
+  ramp-up: 1m
+  iterations: 1000
+  scenario: example
 
+scenarios:
+  example:
+    default-address: http://blazedemo.com
+    script: sample.py
+"""
+
+# For more information go to:  https://gettaurus.org/docs/Locust/
 
