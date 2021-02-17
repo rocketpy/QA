67e431e1fbc0e8fa451ecc6e1b4fbdc8bd2c774b
rocketpy <dimitr.ushakov@gmail.com>
2021-02-17 21:40:57 +0200

    Update Taurus_testing.py

--- Taurus_testing.py
+++ Taurus_testing.py
@@ -1,1 +1,9 @@
+#  Taurus Tool for Continuous Testing , Automation-friendly framework for Continuous Testing !
+
+
+#  PyPi:  https://pypi.org/project/bzt/
+#  Docs:  https://gettaurus.org/docs/Index/
+
+#  pip install bzt
+
 
