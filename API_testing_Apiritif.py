3814c0faf803a042db43281695f07bd58706fc9f
rocketpy <dimitr.ushakov@gmail.com>
2021-04-27 22:37:06 +0300

    Update API_testing_Apiritif.py

--- API_testing_Apiritif.py
+++ API_testing_Apiritif.py
@@ -1,1 +1,16 @@
+#  Apiritif - Python framework for API testing
+
+# PyPi:  https://pypi.org/project/apiritif/
+# Github: https://github.com/Blazemeter/apiritif
+
+
+#  HTTP Requests
+# Apiritif allows to use simple requests-like API for making HTTP requests.
+
+from apiritif import http
+
+response = http.get("http://...")
+response.assert_ok()  # will raise AssertionError if request wasn't successful
+
+
 
