6ce1d58bdfc870c4a77483bbb91b909cab0da1b0
rocketpy <dimitr.ushakov@gmail.com>
2021-04-27 22:39:13 +0300

    Update API_testing_Apiritif.py

--- API_testing_Apiritif.py
+++ API_testing_Apiritif.py
@@ -13,4 +13,28 @@ response = http.get("http://...")
 response.assert_ok()  # will raise AssertionError if request wasn't successful
 
 
+#  http object provides the following methods:
+
+from apiritif import http
+
+http.get("http://api.example.com/posts")
+http.post("http://api.example.com/posts")
+http.put("http://api.example.com/posts/1")
+http.patch("http://api.example.com/posts/1")
+http.delete("http://api.example.com/posts/1")
+http.head("http://api.example.com/posts")
+# All methods (get, post, put, patch, delete, head) support the following arguments:
+
+def get(address,               # URL for the request
+        params=None,           # URL params dict
+        headers=None,          # HTTP headers
+        cookies=None,          # request cookies
+        data=None,             # raw request data
+        json=None,             # attach JSON object as request body
+        encrypted_cert=None,   # certificate to use with request 
+        allow_redirects=True,  # automatically follow HTTP redirects
+        timeout=30)            # request timeout, by default it's 30 seconds
+
+
+#  for more info go to Docs
 
