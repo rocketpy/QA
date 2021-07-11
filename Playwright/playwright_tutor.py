2a757f582e31e2a1a764bb35c59e6aeef5acad9c
rocketpy <dimitr.ushakov@gmail.com>
2021-07-11 23:34:19 +0300

    Update playwright_tutor.py

--- Playwright/playwright_tutor.py
+++ Playwright/playwright_tutor.py
@@ -3,6 +3,8 @@
 # PyPi:  https://pypi.org/project/playwright/
 # pip install playwright
 
+# PyTest and Playwright:  https://github.com/microsoft/playwright-pytest
+
 # Checking for sync API
 
 # This is our default API for short snippets and tests. If you are not using asyncio in your application, it is the easiest to use Sync API notation.
@@ -50,5 +52,24 @@ def test_playwright_is_visible_on_google(page):
     page.type("input[name=q]", "Playwright GitHub")
     page.click("input[type=submit]")
     page.wait_for_selector("text=microsoft/Playwright")
+
+
+# Interactive mode (REPL)
+# Blocking REPL, as in CLI:
+
+from playwright.sync_api import sync_playwright
+
+
+playwright = sync_playwright().start()
+
+# Use playwright.chromium, playwright.firefox or playwright.webkit
+# Pass headless=False to see the browser UI
+browser = playwright.chromium.launch()
+page = browser.new_page()
+page.goto("http://whatsmyuseragent.org/")
+page.screenshot(path="example.png")
+browser.close()
+playwright.stop()
+
 
 
