be62f099dd29e8333d0e5bc32fc799e3f4359e47
rocketpy <dimitr.ushakov@gmail.com>
2021-07-13 23:51:41 +0300

    Update Allure_PyTest.py

--- Allure_PyTest.py
+++ Allure_PyTest.py
@@ -1,3 +1,12 @@
+import time
+import pytest
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
 driver = webdriver.Firefox()
 
 @allure.feature('Функциональное тестирование')
