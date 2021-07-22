2aeb5c79dd103e939507c092722e31f975d65b0f
rocketpy <dimitr.ushakov@gmail.com>
2021-07-22 23:29:02 +0300

    Update appium_example_test_2.py

--- appium_example_test_2.py
+++ appium_example_test_2.py
@@ -1,1 +1,11 @@
+from appium import webdriver
+from config import url
 
+
+URL = 'https://www...'
+
+DESIRE_CAPABILITY = {'platformName': 'iOS',
+                     'platformVersion': '11.4',
+                     'deviceName': 'iPhone X',
+                     'browserName': 'Safari'
+                    }
