e6ad857923db11c0f046ec094313e04270577adb
rocketpy <dimitr.ushakov@gmail.com>
2021-07-26 23:37:29 +0300

    Update Stress_Testing_HTTP.py

--- Stress_Testing_HTTP.py
+++ Stress_Testing_HTTP.py
@@ -1,1 +1,14 @@
+from treq import get
 
+
+def done(response):
+    print(response.code)
+    reactor.stop()
+    
+get("http://www...").addCallback(done)
+
+
+from twisted.internet import reactor
+
+reactor.run() 
+# 200
