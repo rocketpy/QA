ee422a5f61f80e05238ac1c8ef24ef2a0f471dcc
rocketpy <dimitr.ushakov@gmail.com>
2021-07-27 23:43:47 +0300

    Update Stress_Testing_HTTP.py

--- Stress_Testing_HTTP.py
+++ Stress_Testing_HTTP.py
@@ -1,10 +1,12 @@
 from treq import get
+# import treq
 
 
 def done(response):
     print(response.code)
     reactor.stop()
     
+# treq.get("https://github.com").addCallback(done) 
 get("http://www...").addCallback(done)
 
 
