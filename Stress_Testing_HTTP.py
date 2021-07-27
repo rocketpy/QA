d632789c13474da08137622bb61921dbcd0037fd
rocketpy <dimitr.ushakov@gmail.com>
2021-07-28 00:23:53 +0300

    Update Stress_Testing_HTTP.py

--- Stress_Testing_HTTP.py
+++ Stress_Testing_HTTP.py
@@ -31,4 +31,20 @@ req_generated = 0
 cooperator = task.Cooperator()
 pool = HTTPConnectionPool(reactor)
     
-def counter():  
+def counter():
+    print("Requests: {} generated; {} made; {} done".format(
+    req_generated, req_made, req_done))
+    # reset the counters and reschedule ourselves
+    req_generated = req_made = req_done = 0
+    reactor.callLater(1, counter)
+    def body_received(body):  
+    global req_done
+    req_done += 1
+    def request_done(response):  
+    global req_made
+    deferred = treq.json_content(response)
+    req_made += 1
+    deferred.addCallback(body_received)
+    deferred.addErrback(lambda x: None)  # ignore errors
+    return deferred
+
