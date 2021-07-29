b3fba9ee340d51abca69cc3ea3f0fa2db93458ac
rocketpy <dimitr.ushakov@gmail.com>
2021-07-29 20:48:23 +0300

    Update unittest_Selenium_test_example.py

--- unittest_Selenium_test_example.py
+++ unittest_Selenium_test_example.py
@@ -5,4 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
 
+class Register(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path="/chromedriver")
+        self.driver.maximize_window()
 
