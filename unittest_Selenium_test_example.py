957caa6678df95b3f2530ad69267cae6417a2f7a
rocketpy <dimitr.ushakov@gmail.com>
2021-07-29 21:09:28 +0300

    Update unittest_Selenium_test_example.py

--- unittest_Selenium_test_example.py
+++ unittest_Selenium_test_example.py
@@ -39,4 +39,23 @@ def test_search_in_python_org(self):
     # a year
     select = Select(driver.find_element_by_id('...'))
     select.select_by_visible_text('...')
+    
+    # email and phone number
+    elem = driver.find_element_by_id("...")
+    elem.send_keys("...")
+    elem = driver.find_element_by_id("...")
+    elem.send_keys("...")
+    
+    # img of profile
+    elem = driver.find_element_by_id("...")
+    elem.send_keys("file name or path to file")
+    
+    # input passwords
+    elem = driver.find_element_by_id("")
+    elem.send_keys("...")
+    elem = driver.find_element_by_id("...")
+    elem.send_keys("...")
+    # time.sleep(5)
+    elem = driver.find_element_by_xpath("...").click()
+
 
