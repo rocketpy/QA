a00bfb20e9844335167b2d35c91bd23f3e792e16
rocketpy <dimitr.ushakov@gmail.com>
2021-07-31 23:53:02 +0300

    Update appium_example_test_3.py

--- appium_example_test_3.py
+++ appium_example_test_3.py
@@ -1,1 +1,13 @@
+from appium import webdriver
 
+ 
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '6.0'
+wanted_caps ['deviceName'] = '1234567890123'
+desired_caps['appPackage'] = 'com.android.settings'
+desired_caps['appActivity'] = '.Settings'
+ 
+driver = webdriver.Remote ('http: // localhost: 4723 / wd / hub', wanted_caps)
+# driver.find_element_by_name("=").click() 
+driver.quit () 
