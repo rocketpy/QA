d104c94edc567137fb23a78ea2276b0a0142356f
rocketpy <dimitr.ushakov@gmail.com>
2021-08-13 23:10:19 +0300

    Update and rename Allure_behave,py to Allure_behave.py

--- Allure_behave.py
+++ Allure_behave.py
@@ -1,6 +1,7 @@
 # Installation and Usage
 
 # pip install allure-behave
+
 # behave -f allure_behave.formatter:AllureFormatter -o %allure_result_folder% ./features
 # allure serve %allure_result_folder%
 
@@ -10,6 +11,7 @@
 # So in this case you need patch your environment.py files instead using formatter. If you don't use environment.py, just crate empty one with calling allure like in example below.
 
 from allure_behave.hooks import allure_report
+
 
 ### your code
 
