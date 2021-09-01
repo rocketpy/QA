c09e54ad919c1ba5e7bbfff34d91d36e134ab20c
rocketpy <dimitr.ushakov@gmail.com>
2021-09-01 23:39:08 +0300

    Update API_testing.py

--- API_testing.py
+++ API_testing.py
@@ -1,1 +1,11 @@
+# pip install -U requests
+# pip install -U pytest
 
+import requests
+
+
+def test_get_200():
+     response = requests.get("http://api...")
+     assert response.status_code == 200
+
+      
