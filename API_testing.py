c9e63d4ad205f92d864cc66f71f79f5e83f17e1b
rocketpy <dimitr.ushakov@gmail.com>
2021-09-03 23:01:02 +0300

    Update API_testing.py

--- API_testing.py
+++ API_testing.py
@@ -10,23 +10,30 @@ def test_get_200():
     response = requests.get("http://api...")
     assert response.status_code == 200
 
-      
+
 # Check if the value of the response content type header correctly
 def test_check_header():
     response = requests.get("http://api...")
     assert response.headers["Content-Type"] == "application/json"
      
-     
+
 # Checking the value of a response body element
 def test_check_body_element():
     response = requests.get("http://api...")
     response_body = response.json()
     assert response_body["country"] == "Ukraine"
 
-     
+
 # Extract and assert on the value of the place name
-def test_():
+def test_some_element():
     response = requests.get("http://api...")
     response_body = response.json()
     assert response_body["places"][0]["place name"] == "Braiton"
-     
+
+
+# Checking len of values
+def test_len_elems():
+    response = requests.get("http://api...")
+    response_body = response.json()
+    assert len(response_body["places"]) == 1
+      
