7e2837e00a4b8c5e98693261a5f68bd5a78d711a
rocketpy <dimitr.ushakov@gmail.com>
2021-09-08 22:50:36 +0300

    Update webbot_tutor.py

--- Webbot/webbot_tutor.py
+++ Webbot/webbot_tutor.py
@@ -25,3 +25,12 @@ web = Browser()
 web.go_to('siteurl.com')
 web.click('buttontext' , multiple = True)
 
+
+# If there are multiple elements and you want to perform action on one of them
+
+web = Browser()
+web.go_to('siteurl.com')
+# types the text into the 3rd input element when there are multiple input elements with form-input class
+web.type('im robo typing' , number = 3 , classname="form-input" )
+web.click('Post')
+
