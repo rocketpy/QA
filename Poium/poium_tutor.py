321ae0d81d15a80e7dbb2d69d54a9232217d7163
rocketpy <dimitr.ushakov@gmail.com>
2021-09-09 20:53:29 +0300

    Update poium_tutor.py

--- Poium/poium_tutor.py
+++ Poium/poium_tutor.py
@@ -1,1 +1,10 @@
+#  Poium - Selenium/appium-based Page Objects test library.
+
+# PyPi: https://pypi.org/project/poium/
+# Github: https://github.com/SeldomQA/poium
+
+# pip install poium
+
+# To keep up with the latest version:
+# pip install -U git+https://github.com/SeldomQA/poium.git@master
 
