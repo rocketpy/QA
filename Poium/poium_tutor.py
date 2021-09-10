5ffbf916598aec83b4766a3d36bcdeda55be1141
rocketpy <dimitr.ushakov@gmail.com>
2021-09-10 22:42:59 +0300

    Update poium_tutor.py

--- Poium/poium_tutor.py
+++ Poium/poium_tutor.py
@@ -8,3 +8,22 @@
 # To keep up with the latest version:
 # pip install -U git+https://github.com/SeldomQA/poium.git@master
 
+# Example
+from poium import Page, Element
+from selenium import webdriver
+
+
+class BaiduIndexPage(Page):
+    search_input = Element(name='wd')
+    search_button = Element(id_='su')
+
+
+driver = webdriver.Chrome()
+page = BaiduIndexPage(driver)
+page.get("https://www.baidu.com")
+
+page.search_input.send_keys("poium") 
+page.search_button.click()
+
+driver.quit()
+
