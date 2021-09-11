7930ee903f84e4d835dbc55acff923b81590c346
rocketpy <dimitr.ushakov@gmail.com>
2021-09-11 22:23:24 +0300

    Update stere_tutor.py

--- Stere/stere_tutor.py
+++ Stere/stere_tutor.py
@@ -1,1 +1,12 @@
+# Stere - A nice way of implementing the Page Object pattern.
+# Stere is a library for writing Page Objects, designed to work on top of an existing automation library.
+
+# PyPi: https://pypi.org/project/stere/
+# Github: https://github.com/jsfehler/stere
+# Docs: https://stere.readthedocs.io/en/latest/
+
+# pip install stere
+
+
+
 
