40fa47c57e2ad39a1b33f8193b1443b39db67333
rocketpy <dimitr.ushakov@gmail.com>
2021-09-23 22:33:03 +0300

    Update golem_tutor.py

--- Golem/golem_tutor.py
+++ Golem/golem_tutor.py
@@ -1,1 +1,16 @@
+#  Golem - Test Automation
 
+# Github: https://github.com/golemhq/golem
+# Docs: https://golem-framework.readthedocs.io/en/latest/
+
+# pip install golem-framework
+
+"""
+Batteries Included:
+    Multi-user web IDE
+    Extended classes for WebDriver and WebElement
+    More than 200 self documenting Actions
+    Webdriver-manager utility
+    Built in parallel test support
+    Reporting engine
+"""
