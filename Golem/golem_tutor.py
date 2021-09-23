9efd530c1d379179248b093bd9f35208508057a9
rocketpy <dimitr.ushakov@gmail.com>
2021-09-23 22:34:34 +0300

    Update golem_tutor.py

--- Golem/golem_tutor.py
+++ Golem/golem_tutor.py
@@ -14,3 +14,18 @@ Batteries Included:
     Built in parallel test support
     Reporting engine
 """
+
+# A test example
+
+# Next is a test that navigates to ‘en.wikipedia.org’, searches an article and validates that the title of the article is correct.
+# validate_article_title.py
+
+description = 'Search an article in Wikipedia'
+
+def test_validate_article_title(data):
+    navigate('http://en.wikipedia.org/')
+    send_keys(('id', 'searchInput'), 'automation')
+    click(('id', 'searchButton'))
+    verify_element_text(('id', 'firstHeading'), 'Automation')
+
+
