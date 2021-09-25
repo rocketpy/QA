56ef1b5ae30e6209dcbedd72c8bf3af36a4f4783
rocketpy <dimitr.ushakov@gmail.com>
2021-09-25 19:34:36 +0300

    Update PyTest_Splinter.py

--- PyTest_Splinter.py
+++ PyTest_Splinter.py
@@ -18,4 +18,15 @@ Fixtures:
         This fixture is required only if you need to have multiple instances of the Browser in a single test at the same time. Example of usage.
 """
 
+@pytest.fixture
+def admin_browser(request, browser_instance_getter):
+    """Admin browser fixture."""
+    # browser_instance_getter function receives parent fixture -- our admin_browser
+    return browser_instance_getter(request, admin_browser)
+
+def test_2_browsers(browser, admin_browser):
+    """Test using 2 browsers at the same time."""
+    browser.visit('http://google.com')
+    admin_browser.visit('http://admin.example.com')
+
 
