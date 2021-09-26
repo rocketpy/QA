6217aa4faa42942073907e94801cf54a7023391a
rocketpy <dimitr.ushakov@gmail.com>
2021-09-26 17:04:13 +0300

    Update selene_tutor.py

--- Selene/selene_tutor.py
+++ Selene/selene_tutor.py
@@ -2,6 +2,9 @@
 
 # Github: https://github.com/yashaka/selene
 
+# Latest stable version:
+# pip install selene
+
 """
 Main features:
     User-oriented API for Selenium Webdriver (code like speak common English)
@@ -14,8 +17,27 @@ Main features:
 from selene.support.shared import browser
 from selene import by, be, have
 
+
 browser.open('https://google.com/ncr')
 browser.element(by.name('q')).should(be.blank)\
     .type('selenium').press_enter()
 browser.all('.srg .g').should(have.size(10))\
     .first.should(have.text('Selenium automates browsers'))
+
+
+# with custom setup
+from selene.support.shared import browser
+from selene import by, be, have
+
+
+browser.config.browser_name = 'firefox'
+browser.config.base_url = 'https://google.com'
+browser.config.timeout = 2
+# browser.config.* = ...
+
+browser.open('/ncr')
+browser.element(by.name('q')).should(be.blank)\
+    .type('selenium').press_enter()
+browser.all('.srg .g').should(have.size(10))\
+    .first.should(have.text('Selenium automates browsers'))
+
