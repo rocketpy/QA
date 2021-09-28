834bc3f343d9b08aa83b5a79b686f48e813306ce
rocketpy <dimitr.ushakov@gmail.com>
2021-09-28 22:35:28 +0300

    Update helium_selenium.py

--- Helium/helium_selenium.py
+++ Helium/helium_selenium.py
@@ -2,6 +2,7 @@
 
 # PyPi: https://pypi.org/project/helium/
 # Github: https://github.com/mherrmann/selenium-python-helium
+
 # Helium cheatsheet: https://github.com/mherrmann/selenium-python-helium/blob/master/docs/cheatsheet.md
 
 # pip install helium
@@ -14,5 +15,24 @@ start_chrome()
 
 # or
 start_chrome('google.com'))
+
+# headless
+start_chrome(headless=True)
+start_chrome('google.com', headless=True)
+# same for Firefox
+
+# Simple example
+from helium import *
+
+
+start_chrome('google.com')
+write('helium selenium github')
+press(ENTER)
+click('mherrmann/helium')
+go_to('github.com/login')
+write('username', into='Username')
+write('password', into='Password')
+click('Sign in')
+kill_browser()
 
 
