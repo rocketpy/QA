23f20fdb39fe666739028c81a428e1566bf1330f
rocketpy <dimitr.ushakov@gmail.com>
2021-09-28 22:33:53 +0300

    Update helium_selenium.py

--- Helium/helium_selenium.py
+++ Helium/helium_selenium.py
@@ -1,1 +1,18 @@
+#  Lighter browser automation based on Selenium.
+
+# PyPi: https://pypi.org/project/helium/
+# Github: https://github.com/mherrmann/selenium-python-helium
+# Helium cheatsheet: https://github.com/mherrmann/selenium-python-helium/blob/master/docs/cheatsheet.md
+
+# pip install helium
+
+from helium import *
+
+
+start_chrome()
+# start_firefox()
+
+# or
+start_chrome('google.com'))
+
 
