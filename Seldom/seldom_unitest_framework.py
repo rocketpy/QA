0edb71328d25dd49fcd928f3c55826ebb8742586
rocketpy <dimitr.ushakov@gmail.com>
2021-10-06 23:08:52 +0300

    Update seldom_unitest_framework.py

--- Seldom/seldom_unitest_framework.py
+++ Seldom/seldom_unitest_framework.py
@@ -40,4 +40,35 @@ class BaiduTest(seldom.TestCase):
 
 if __name__ == '__main__':
     seldom.main()
+    
+    
+# HTTP
+# seldom 2.0
+
+import seldom
+
+
+class TestRequest(seldom.TestCase):
+
+    def test_put_method(self):
+        self.put('/put', data={'key': 'value'})
+        self.assertStatusCode(200)
+
+    def test_post_method(self):
+        self.post('/post', data={'key':'value'})
+        self.assertStatusCode(200)
+
+    def test_get_method(self):
+        payload = {'key1': 'value1', 'key2': 'value2'}
+        self.get("/get", params=payload)
+        self.assertStatusCode(200)
+
+    def test_delete_method(self):
+        self.delete('/delete')
+        self.assertStatusCode(200)
+
+
+if __name__ == '__main__':
+    seldom.main(base_url="http://httpbin.org")
+
 
