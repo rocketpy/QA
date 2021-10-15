ea40cd5fc1d41db4ea966c164dec4cfa7d16280f
rocketpy <dimitr.ushakov@gmail.com>
2021-10-15 23:28:41 +0300

    Update qweb_tutorial.py

--- Qweb/qweb_tutorial.py
+++ Qweb/qweb_tutorial.py
@@ -21,3 +21,24 @@ Basic interaction
     ClickText           Add to cart                                     # Click *button* with specific text
     DropDown            Size            Large                           # Select value (Large) from dropdown (Size)
 """
+
+"""
+# Timeouts and anchors
+By default QWeb tries to locate the element 10 seconds (default time can be configured).
+Timeout can also be individually given for each keyword as an argument.
+
+When text to be found is not unique, an 'anchor' argument can be given to pinpoint which instance of text we want
+to interact with. Anchor can be either another text nearby or index.
+
+ClickText   Sign-in
+ClickText   Sign-in     timeout=30
+
+ClickText   Sign-in     anchor=Email
+ClickText   Sign-in     index=3
+
+# Other locators
+# Non-textual locators can be used with ClickElementand ClickItemkeywords.
+
+ClickElement    xpath\=//button[@class="my_class"]   # xpath
+ClickItem       Increment quantity                   # alt text
+"""
