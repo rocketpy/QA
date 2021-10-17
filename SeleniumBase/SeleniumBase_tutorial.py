567f470a1d2b4cf88e78411c336c127ec165a8a3
rocketpy <dimitr.ushakov@gmail.com>
2021-10-17 22:23:37 +0300

    Update SeleniumBase_tutorial.py

--- SeleniumBase/SeleniumBase_tutorial.py
+++ SeleniumBase/SeleniumBase_tutorial.py
@@ -1,1 +1,17 @@
+# SeleniumBase - A complete web automation framework for end-to-end testing.
 
+# PyPi: https://pypi.org/project/seleniumbase/
+# Github: https://github.com/seleniumbase/SeleniumBase
+
+# Install SeleniumBase:
+
+# from GitHub:
+git clone https://github.com/seleniumbase/SeleniumBase.git
+cd SeleniumBase/
+pip install .  # Normal installation
+pip install -e .  # Editable install
+
+# (When using a virtual env, the Editable install is faster.)
+
+# from pypi:
+# pip install seleniumbase
