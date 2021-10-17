8e87dd3e3a81fb858ca4a8c5ea003c59fba19ead
rocketpy <dimitr.ushakov@gmail.com>
2021-10-17 22:35:36 +0300

    Update SeleniumBase_tutorial.py

--- SeleniumBase/SeleniumBase_tutorial.py
+++ SeleniumBase/SeleniumBase_tutorial.py
@@ -6,12 +6,32 @@
 # Install SeleniumBase:
 
 # from GitHub:
-git clone https://github.com/seleniumbase/SeleniumBase.git
-cd SeleniumBase/
-pip install .  # Normal installation
-pip install -e .  # Editable install
+# git clone https://github.com/seleniumbase/SeleniumBase.git
+# cd SeleniumBase/
+# pip install .  # Normal installation
+# pip install -e .  # Editable install
 
 # (When using a virtual env, the Editable install is faster.)
 
 # from pypi:
 # pip install seleniumbase
+
+
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+    def test_basics(self):
+        url = "https://store.xkcd.com/collections/posters"
+        self.open(url)
+        self.type('input[name="q"]', "xkcd book")
+        self.click('input[value="Search"]')
+        self.assert_text("xkcd: volume 0", "h3")
+        self.open("https://xkcd.com/353/")
+        self.assert_title("xkcd: Python")
+        self.assert_element('img[alt="Python"]')
+        self.click('a[rel="license"]')
+        self.assert_text("free to copy and reuse")
+        self.go_back()
+        self.click_link("About")
+        self.assert_exact_text("xkcd.com", "h2")
