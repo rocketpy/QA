9435cbad07ecf3eb5d6c8d153fda148da9d601e1
rocketpy <dimitr.ushakov@gmail.com>
2021-10-21 22:38:23 +0300

    Update toolium_tutor.py

--- Toolium/toolium_tutor.py
+++ Toolium/toolium_tutor.py
@@ -1,1 +1,12 @@
+#  Toolium - Wrapper tool of Selenium and Appium libraries to test web and mobile applications in a single project
+
+# PyPI: https://pypi.org/project/toolium/
+# Github: https://github.com/telefonica/toolium
+
+# pip install toolium
+
+# The main dependencies are:
+    # Selenium: to test web applications in major browsers (Firefox, Chrome, Internet Explorer, Edge, Safari, Opera)
+    # Appium-Python-Client: to test mobile applications (native, hybrid or web) in Android or iOS devices/emulators.
+    # requests: to test APIs
 
