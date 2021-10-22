aa0e5f17cbb27fe22ff064b06d12dedb776185b1
rocketpy <dimitr.ushakov@gmail.com>
2021-10-22 22:56:36 +0300

    Update RPA_framework_tutor.py

--- RPA_Framework/RPA_framework_tutor.py
+++ RPA_Framework/RPA_framework_tutor.py
@@ -1,1 +1,6 @@
+# RPA Framework is a collection of open-source libraries and tools for Robotic Process Automation (RPA)
 
+# PyPI: https://pypi.org/project/rpaframework/
+
+# Github: https://www.github.com/robocorp/rpaframework/
+# Docs: https://rpaframework.org/
