43ae19e15ba13dc900595efa6a216b0f2eb59c94
rocketpy <dimitr.ushakov@gmail.com>
2021-10-23 20:07:28 +0300

    Update web_scraper_example.py

--- RPA_Framework/web_scraper_example.py
+++ RPA_Framework/web_scraper_example.py
@@ -1,1 +1,15 @@
+# Web scraper Python robot example
+from RPA.Browser.Selenium import Selenium
 
+
+browser_lib = Selenium()
+
+
+def open_the_website(url):
+    browser_lib.open_available_browser(url)
+
+
+def search_for(term):
+    input_field = "css:input"
+    browser_lib.input_text(input_field, term)
+    browser_lib.press_keys(input_field, "ENTER")
