47143883f3e17d65ca545bdb82c977a55d12fed7
rocketpy <dimitr.ushakov@gmail.com>
2021-10-24 21:40:17 +0300

    Update web_scraper_example.py

--- RPA_Framework/web_scraper_example.py
+++ RPA_Framework/web_scraper_example.py
@@ -13,3 +13,17 @@ def search_for(term):
     input_field = "css:input"
     browser_lib.input_text(input_field, term)
     browser_lib.press_keys(input_field, "ENTER")
+    
+
+def store_screenshot(filename):
+    browser_lib.screenshot(filename=filename)
+
+
+# Define a main() function that calls the other functions in order:
+def main():
+    try:
+        open_the_website("https://robocorp.com/docs/")
+        search_for("python")
+        store_screenshot("output/screenshot.png")
+    finally:
+        browser_lib.close_all_browsers()
