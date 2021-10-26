9fbebadd980423be7ca3bd90f0e02b8eacc24178
rocketpy <dimitr.ushakov@gmail.com>
2021-10-26 22:13:40 +0300

    Update RPA_framework_tutor.py

--- RPA_Framework/RPA_framework_tutor.py
+++ RPA_Framework/RPA_framework_tutor.py
@@ -4,3 +4,28 @@
 
 # Github: https://www.github.com/robocorp/rpaframework/
 # Docs: https://rpaframework.org/
+
+
+# some example Robot Framework code
+*** Settings ***
+Documentation     Robot to enter weekly sales data into the RobotSpareBin Industries Intranet.
+Library           RPA.Browser.Selenium
+
+*** Variables ***
+${URL}=    https://robotsparebinindustries.com/
+
+*** Keywords ***
+Open The Intranet Website
+    Open Available Browser    ${URL}
+
+*** Keywords ***
+Log In
+    Input Text    username    maria
+    Input Password    password    thoushallnotpass
+    Submit Form
+
+*** Tasks ***
+Open the intranet site and log in
+    Open The Intranet Website
+    Log In
+
