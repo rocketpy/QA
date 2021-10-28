8e79d91a71a9dc2c4460488ca9c80a73b141b7c1
rocketpy <dimitr.ushakov@gmail.com>
2021-10-28 21:59:52 +0300

    Update quick_start.py

--- Seldom/quick_start.py
+++ Seldom/quick_start.py
@@ -1,1 +1,23 @@
+# seldom -install chrome
+# seldom -install firefox
+
+# main() method is seldom run test entry method, It provides some of the most basic and important configurations.
+import seldom
+
+# ...
+
+if __name__ == '__main__':
+
+    seldom.main(path="./",
+                browser="chrome",
+                base_url="",
+                report=None,
+                title="project name",
+                description="Environment description",
+                debug=False,
+                rerun=0,
+                save_last_run=False,
+                timeout=None,
+    )
+
 
