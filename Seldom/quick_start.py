504104013799af09b0d743ab16bd96be00034927
rocketpy <dimitr.ushakov@gmail.com>
2021-10-29 23:14:37 +0300

    Update quick_start.py

--- Seldom/quick_start.py
+++ Seldom/quick_start.py
@@ -20,4 +20,27 @@ if __name__ == '__main__':
                 timeout=None,
     )
 
+# Run Test (Run under a terminal (recommended))
+
+# Create the file run.py, And import main() method.
+
+import seldom
+
+
+seldom.main()
+# main() Method Run the use case in the current file by default.
+
+# python run.py      # Run with the Python command
+# seldom -r run.py   # Run with the Seldom command
+
+# Run a class or method
+# The seldom -m command can provide a more granular run.
+
+# seldom -m test_sample     #  test_sample.py file
+# seldom -m test_sample.SampleTest      #  SampleTest Class
+# seldom -m test_sample.SampleTest.test_case    # test_case method
+
+
+
+
 
