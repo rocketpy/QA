c2e2e4315d9cefe50a219993e003ee8e402a98df
rocketpy <dimitr.ushakov@gmail.com>
2021-10-31 20:20:32 +0200

    Update OpenSDK_tutor.py

--- OpenSDK/OpenSDK_tutor.py
+++ OpenSDK/OpenSDK_tutor.py
@@ -1,1 +1,9 @@
+#  OpenSDK - Test Automation platform for Web, Mobile and API testing.
+
+# PyPI: https://pypi.org/project/testproject-python-sdk/
+# Docs: https://testproject.io/
+# Github: https://github.com/testproject-io/python-opensdk
+
+# pip install testproject-python-sdk
+
 
