887ecaf9b538f89aa1969ada9b16f9ea517ecb99
rocketpy <dimitr.ushakov@gmail.com>
2021-11-02 22:22:11 +0200

    Update httprunner_tutor.py

--- Httprunner/httprunner_tutor.py
+++ Httprunner/httprunner_tutor.py
@@ -1,1 +1,15 @@
+# Httprunner - One-stop solution for HTTP(S) testing.
+# HttpRunner is a simple & elegant, yet powerful HTTP(S) testing framework.
 
+# PyPi: https://pypi.org/project/httprunner/
+# Github: https://github.com/httprunner/httprunner
+
+# pip install httprunner
+
+"""
+Design Philosophy:
+-  Convention over configuration
+-  ROI matters
+-  Embrace open source, leverage requests, pytest, pydantic, allure and locust.
+
+"""
