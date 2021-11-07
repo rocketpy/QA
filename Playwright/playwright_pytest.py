51a101db02c3d537ab29709a291b7a768753b21b
rocketpy <dimitr.ushakov@gmail.com>
2021-11-07 21:57:08 +0200

    Update playwright_pytest.py

--- Playwright/playwright_pytest.py
+++ Playwright/playwright_pytest.py
@@ -1,1 +1,8 @@
+# Pytest plugin for Playwright, a pytest wrapper with fixtures for Playwright to automate web browsers
 
+# PyPi: https://pypi.org/project/pytest-playwright/
+# Github: https://github.com/microsoft/playwright-pytest
+# Docs: https://playwright.dev/python/docs/test-runners/
+
+
+# pip install pytest-playwright
