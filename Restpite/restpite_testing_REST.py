4754615fd970aebf323b928c6485b947bcc5f318
rocketpy <dimitr.ushakov@gmail.com>
2021-11-08 22:23:24 +0200

    Update restpite_testing_REST.py

--- Restpite/restpite_testing_REST.py
+++ Restpite/restpite_testing_REST.py
@@ -1,1 +1,12 @@
+# Python DSL for easily testing REST services
+
+# PyPi: https://pypi.org/project/restpite/
+# Github: https://github.com/symonk/restpite
+# Docs: pip install restpite
+
+# pip install restpite
+# git clone git://github.com/symonk/restpite
+
+
+
 
