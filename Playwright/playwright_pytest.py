2932cc8fa593221b2ccf11bae460d7ddf321ecf0
rocketpy <dimitr.ushakov@gmail.com>
2021-11-09 21:48:10 +0200

    Update playwright_pytest.py

--- Playwright/playwright_pytest.py
+++ Playwright/playwright_pytest.py
@@ -7,8 +7,24 @@
 
 # pip install pytest-playwright
 
+# Usage#
+
+# pip install pytest-playwright
+
+Use the page fixture to write a basic test. See more examples.
+
+# test_my_application.pydef test_example_is_working(page):
+# page.goto("https://example.com")    assert page.inner_text('h1') == 'Example Domain'    page.click("text=More information")
+
+# To run your tests, use pytest CLI.
+
+# Run tests (Chromium and headless by default)pytest
+# Run tests in headed modepytest --headed
+# Run tests in a different browser (chromium, firefox, webkit)pytest --browser firefox
+# Run tests in multiple browserspytest --browser chromium --browser webkit
+
 # Examples:
-# Configure Mypy typings for auto-completion#
+# Configure Mypy typings for auto-completion
 
 # test_my_application.pyfrom playwright.sync_api import Page
 def test_visit_admin_dashboard(page: Page):
