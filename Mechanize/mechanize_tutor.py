19aea00769618c69d11c95e314caff28bb02bb4e
rocketpy <dimitr.ushakov@gmail.com>
2021-11-12 22:48:58 +0200

    Update mechanize_tutor.py

--- Mechanize/mechanize_tutor.py
+++ Mechanize/mechanize_tutor.py
@@ -1,1 +1,19 @@
+#  mechanize - Automate interaction with HTTP web servers
 
+# PyPi:https://pypi.org/project/mechanize/
+# Github: https://github.com/python-mechanize/mechanize
+
+# pip install mechanize
+
+"""
+Major features:
+
+Stateful programmatic web browsing in Python
+  The browser class mechanize.Browser implements the interface of urllib2.OpenerDirector, so any URL can be opened not just http.
+  Easy HTML form filling.
+  Convenient link parsing and following.
+  Browser history (.back() and .reload() methods).
+  The Referer HTTP header is added properly (optional).
+  Automatic observance of robots.txt.
+  Automatic handling of HTTP-Equiv and Refresh.
+"""
