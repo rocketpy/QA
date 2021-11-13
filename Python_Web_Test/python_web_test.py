9d8fc29d406b40eb17daeb0fd24534962a4c1789
rocketpy <dimitr.ushakov@gmail.com>
2021-11-13 22:11:47 +0200

    Update python_web_test.py

--- Python_Web_Test/python_web_test.py
+++ Python_Web_Test/python_web_test.py
@@ -1,1 +1,20 @@
+#  Selene + Pytest tests project template
+
+# Github: https://github.com/yashaka/python-web-test
+
+# Installation
+
+   # pyenv + python: https://github.com/pyenv/pyenv
+   # poetry: https://poetry.eustace.io/docs/#installation
+   # allure: https://docs.qameta.io/allure/#_installing_a_commandline
+  
+# Clone template with: git clone
+
+# in your favourite terminal:
+# cd $YOUR_PROJECT_FOLDER_PATH
+# poetry install
+# poetry shell
+
+# run your tests via:
+# pytest tests/
 
