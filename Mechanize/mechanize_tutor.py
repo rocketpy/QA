b19cff9cfafea658ae7bd62db1aed790cc15dc20
rocketpy <dimitr.ushakov@gmail.com>
2021-11-17 23:29:29 +0200

    Update mechanize_tutor.py

--- Mechanize/mechanize_tutor.py
+++ Mechanize/mechanize_tutor.py
@@ -36,3 +36,14 @@ print(response1.geturl())
 print(response1.info())  # headers
 print(response1.read())  # body
 
+br.select_form(name="order")
+# Browser passes through unknown attributes (including methods)
+# to the selected HTMLForm.
+br["cheeses"] = ["mozzarella", "caerphilly"]  # (the method here is __setitem__)
+# Submit current form.  Browser calls .close() on the current response on
+# navigation, so this closes response1
+response2 = br.submit()
+
+# print currently selected form (don't call .submit() on this, use br.submit())
+print(br.form)
+
