479a437a265bd0c3f823304a1c8ef926751af5a3
rocketpy <dimitr.ushakov@gmail.com>
2021-12-26 23:29:12 +0200

    Update Pytest_tricks.py

--- Pytest_tricks.py
+++ Pytest_tricks.py
@@ -13,4 +13,44 @@ Here’s how the command-line is built in the presence of addopts or the environ
 <pytest.ini:addopts> $PYTEST_ADDOPTS <extra command-line arguments>
 """
 
+# So if the user executes in the command-line:
+#  pytest -m slow
+
+# The actual command line executed is:
+#  pytest -ra -q -v -m slow
+
+# Note that as usual for other command-line applications, in case of conflicting options the last one wins,
+# so the example above will show verbose output because -v overwrites -q.
+
+
+#  Pass different values to a test function, depending on command line options
+# Suppose we want to write a test that depends on a command line option. Here is a basic pattern to achieve this:
+
+# content of test_sample.py
+def test_answer(cmdopt):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
+    assert 0  # to see what was printed
+
+For this to work we need to add a command line option and provide the cmdopt through a fixture function:
+
+# content of conftest.py
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+    )
+
+
+@pytest.fixture
+def cmdopt(request):
+    return request.config.getoption("--cmdopt")
+
+# run this without supplying our new option:
+$ pytest -q test_sample.py
+
 
