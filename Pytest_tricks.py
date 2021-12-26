b2e108f57dc39e5b2aa52f8fe9c5531a074c0c36
rocketpy <dimitr.ushakov@gmail.com>
2021-12-26 23:26:45 +0200

    Update Pytest_tricks.py

--- Pytest_tricks.py
+++ Pytest_tricks.py
@@ -1,1 +1,16 @@
+#  How to change command line options
+
+# content of pytest.ini
+# addopts = -ra -q
+
+"""
+Alternatively, you can set a PYTEST_ADDOPTS environment variable to add command line options while the environment is in use:
+
+export PYTEST_ADDOPTS="-v"
+
+Here’s how the command-line is built in the presence of addopts or the environment variable:
+
+<pytest.ini:addopts> $PYTEST_ADDOPTS <extra command-line arguments>
+"""
+
 
