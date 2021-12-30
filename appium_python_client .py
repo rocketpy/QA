da4a5b781ababb86ea76204af4074cb18701d22d
rocketpy <dimitr.ushakov@gmail.com>
2021-12-30 23:47:11 +0200

    Update appium_python_client .py

--- appium_python_client .py
+++ appium_python_client .py
@@ -113,3 +113,20 @@ self.driver = webdriver.Remote('http://localhost:4723/wd/hub',
                                desired_caps, direct_connection=True)
 
 
+#  SSL validation
+# strict_ssl option allows you to send commands to an invalid certificate host like a self-signed one.
+
+import unittest
+from appium import webdriver
+
+
+desired_caps = dict(
+    platformName='iOS',
+    platformVersion='13.4',
+    automationName='xcuitest',
+    deviceName='iPhone Simulator',
+    app=PATH('../../apps/UICatalog.app.zip')
+)
+
+self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps, strict_ssl=False)
+
